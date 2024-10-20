bab4e7b2aae30b79d9686854c1d13e4a86896723
Samara Augustin <samaraaugust123@gmail.com>
2024-10-20 18:26:47 -0400

    fixed some more flake issues

--- polls/migrations/0001_initial.py
+++ polls/migrations/0001_initial.py
@@ -25,7 +25,9 @@ class Migration(migrations.Migration):
             name='Choice',
             fields=[
                 ('id', models.BigAutoField(
-                    auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                    auto_created=True,
+                    primary_key=True,
+                    serialize=False, verbose_name='ID')),
                 ('choice_text', models.CharField(max_length=200)),
                 ('votes', models.IntegerField(default=0)),
                 ('question', models.ForeignKey(
